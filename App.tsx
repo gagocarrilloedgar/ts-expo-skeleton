1c94e6e5210ead7eb4f55021f701d7c029e9ff3f
Edgar Gago Carrillo <edgargc.upc@gmail.com>
2022-10-24 18:53:34 +0200

    Initial commit
    Generated by create-expo-app 1.1.2.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -11,9 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    color: "red",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
